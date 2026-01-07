fa63a725b9f4af3a64d696a8763728cf15d06fd9
sefatm <sefatmahmud995@gmail.com>
2026-01-07 13:11:35 +0600

    empmgt1

---
+++ src/emmgt1/model/Employee.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emmgt1.model;
+
+/**
+ *
+ * @author user
+ */
+public class Employee {
+    private int id;
+    private String fname;
+    private String lname;
+    private String designation;
+    private String department;
+    private double salary;
+    private int age;
+    private String addrss;
+
+    public Employee(int id, String fname, String lname, String designation, String department, double salary, int age, String addrss) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.designation = designation;
+        this.department = department;
+        this.salary = salary;
+        this.age = age;
+        this.addrss = addrss;
+    }
+
+    public Employee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddrss() {
+        return addrss;
+    }
+
+    public void setAddrss(String addrss) {
+        this.addrss = addrss;
+    }
+    
+    
+    
+}
